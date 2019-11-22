5cc15ef08f7ecb076e08b63c69cd23ddf81f83c6
dhiraj <dhiraj0641@gmail.com>
2019-11-22 22:03:58 +0530

    second commit

---
+++ js/main.js
@@ -0,0 +1,108 @@
+$(document).ready(function(){
+
+	TweenMax.set(['.anim-00 img, .phone-screen_anim-02'], {autoAlpha: 1});
+	
+
+	function getMobileSlide01Tl(){
+		var mobileSlide01Tl = new TimelineMax();
+
+		return mobileSlide01Tl;
+
+	}
+
+	function getMobileSlide02Tl(){
+		var mobileSlide02Tl = new TimelineMax();
+
+		mobileSlide02Tl
+				    // .fromTo('#phonescr1' , 2, {x:0,autoAlpha:1,rotation:0}, {x:-486,autoAlpha:1,rotation:90})
+
+					.fromTo('.phone-screen_anim-03', 2, {x:0,autoAlpha:1,rotation:0}, {x:-470,autoAlpha:1,rotation:90});
+
+
+			return mobileSlide02Tl;
+
+	}					
+	
+
+	function getMobileSlide03Tl(){
+		var mobileSlide03Tl = new TimelineMax();
+
+		mobileSlide03Tl
+			
+			.fromTo('.phone-screen_anim-04', 1, {y: '+=38', autoAlpha: 0}, {y:'0', autoAlpha: 1})
+
+			
+
+		return mobileSlide03Tl;
+
+	}		
+	
+	enquire.register("screen and (min-width: 769px)", {
+
+		match: function(){
+
+			initScrollMagic();
+
+		},
+		unmatch: function(){
+
+			controller.destroy(true);
+			$('*').removeAttr('style');
+
+		}
+
+	})
+
+	function initScrollMagic(){
+
+		// Init ScrollMagic
+		controller = new ScrollMagic.Controller();
+
+
+	// Pin Mobile Screen
+		var pinMobile = new ScrollMagic.Scene({
+			triggerElement: '#phone-floater-container',
+			triggerHook: 0,
+			duration: '1600'
+		})
+		.setPin('#phone-floater-container .pin-wrapper', {pushFollowers: false})
+		// .addIndicators()
+		.addTo(controller);
+
+		// Pin Mobile Scene 1
+		var pinScene01 = new ScrollMagic.Scene({
+			triggerElement: '#phone-floater-container',
+			triggerHook: 0.4,
+			duration: '100'
+		})
+		.setTween(getMobileSlide01Tl())
+		// .addIndicators()
+		.addTo(controller);
+
+		// Pin Mobile Scene 2
+		var pinScene02 = new ScrollMagic.Scene({
+			triggerElement: '#phone-floater-container',
+			triggerHook: 0,
+			offset: 450,
+			duration: '0'
+		})
+		.setTween(getMobileSlide02Tl())
+		// .addIndicators()
+		.addTo(controller);
+
+
+		// Pin Mobile Scene 3
+		var pinScene03 = new ScrollMagic.Scene({
+			triggerElement: '#phone-floater-container',
+			triggerHook: 0,
+			offset: 1250,
+			duration: '200'
+		})
+		.setTween(getMobileSlide03Tl())
+		// .addIndicators()
+		.addTo(controller);
+
+	}
+
+
+});
